0ae763b9b47253c04e3ed96dc4ba9b7fe0686c5f
VladislavRb <vlad.raba@mail.ru>
2020-11-22 20:13:03 +0300

    write triangle tests

--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -1,7 +1,14 @@
+import org.junit.jupiter.api.BeforeAll;
+
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import triangleException.WrongInputException;
 import triangleUtils.Triangle;
 
 import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TriangleTest {
     private static Triangle triangle;
@@ -74,5 +81,101 @@ public class TriangleTest {
                 Arguments.of((Object) new Object[] {2, 5, 6}),
                 Arguments.of((Object) new Object[] {4, 7, 4})
         );
+    }
+
+    @BeforeAll
+    static void initTriangle() {
+        triangle = new Triangle();
+    }
+
+    @ParameterizedTest(name = "wrong arguments length")
+    @MethodSource("provideArgumentsForWrongArgumentsLengthException")
+    void wrongArgumentsLengthException(Object ...args) {
+        Exception exception = assertThrows(WrongInputException.class, () -> {
+            triangle.isTriangle(args);
+        });
+
+        String expectedMessage = "3 arguments should be passed";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(actualMessage, expectedMessage);
+    }
+
+    @ParameterizedTest(name = "wrong arguments type")
+    @MethodSource("provideArgumentsForWrongArgumentsTypeException")
+    void wrongArgumentsTypeException(Object ...args) {
+        Exception exception = assertThrows(WrongInputException.class, () -> {
+            triangle.isTriangle(args);
+        });
+
+        String expectedMessage = "Wrong type of one ore more arguments";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(actualMessage, expectedMessage);
+    }
+
+    @ParameterizedTest(name = "zero sides")
+    @MethodSource("provideArgumentsForZeroArgumentsException")
+    void zeroArgumentsException(Object... args) {
+        Exception exception = assertThrows(WrongInputException.class, () -> {
+            triangle.isTriangle(args);
+        });
+
+        String expectedMessage = "A side value can't be equal to 0";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(actualMessage, expectedMessage);
+    }
+
+    @ParameterizedTest(name = "negative sides")
+    @MethodSource("provideArgumentsForNegativeArgumentsException")
+    void negativeArgumentsException(Object... args) {
+        Exception exception = assertThrows(WrongInputException.class, () -> {
+            triangle.isTriangle(args);
+        });
+
+        String expectedMessage = "A side value can't be negative";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(actualMessage, expectedMessage);
+    }
+
+    @ParameterizedTest(name = "is not a triangle")
+    @MethodSource("provideArgumentsForImpossibleTriangle")
+    void impossibleTriangle(Object... args) throws WrongInputException {
+        assertFalse(triangle.isTriangle(args));
+    }
+
+    @ParameterizedTest(name = "is a triangle")
+    @MethodSource("provideArgumentsForPossibleTriangle")
+    void possibleTriangle(Object... args) throws WrongInputException {
+        assertTrue(triangle.isTriangle(args));
+    }
+
+    @ParameterizedTest(name = "triangle is acute")
+    @MethodSource("provideArgumentsForAcuteTriangle")
+    void acuteTriangle(Object ...args) throws WrongInputException {
+        String expected = "acute";
+        String actual = triangle.getType(args);
+
+        assertEquals(actual, expected);
+    }
+
+    @ParameterizedTest(name = "triangle is right")
+    @MethodSource("provideArgumentsForRightTriangle")
+    void rightTriangle(Object ...args) throws WrongInputException {
+        String expected = "right";
+        String actual = triangle.getType(args);
+
+        assertEquals(actual, expected);
+    }
+
+    @ParameterizedTest(name = "triangle is obtuse")
+    @MethodSource("provideArgumentsForObtuseTriangle")
+    void obtuseTriangle(Object ...args) throws WrongInputException {
+        String expected = "obtuse";
+        String actual = triangle.getType(args);
+
+        assertEquals(actual, expected);
     }
 }
